3b04659645d5caa0bcc72539c7b6a4efe8adef93
favour <chibueze.favour.o@gmail.com>
2022-02-17 09:27:38 +0100

    fixed modal

--- src/js/app.js
+++ src/js/app.js
@@ -36,12 +36,14 @@ modalBtn.onclick = function() {
 
 // When the user clicks on <span> (x), close the modal
 modalSpan.onclick = function() {
+    
     modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
+        modal.className += "out";
         modal.style.display = "none";
     }
 }
